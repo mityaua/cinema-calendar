d9df80211a3261f3820f72600ca5cddc22f6afa4
Dmitriy Shukaliuk <mityaua@gmail.com>
2021-03-02 11:08:26 +0200

    Fixes styles and loader for poster

--- src/js/components/lightbox.js
+++ src/js/components/lightbox.js
@@ -1,22 +1,26 @@
 import * as basicLightbox from 'basiclightbox';
 import 'basiclightbox/dist/basicLightbox.min.css';
 
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+
 import { listRef } from '../references/refs';
 
 listRef.addEventListener('click', openModal);
 
 // Open lightbox
 function openModal(event) {
-  // event.preventDefault();
-
   if (event.target.nodeName !== 'IMG') {
     return;
   }
 
+  NProgress.start();
+
   const img = `<img src= ${event.target.src}>`;
   const instance = basicLightbox.create(img);
-
   instance.show();
+
+  NProgress.done(true);
   window.addEventListener('keydown', closeModal);
 
   // Close lightbox by Escaoe
